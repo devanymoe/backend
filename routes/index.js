e4fa0a6f641d50f6a8d90fefdc75bb02a7b63613
Max Dobler <mdblr@outlook.com>
2016-06-16 15:18:10 -0700

    make exchange with facebook to log user in to app

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,35 @@
 var express = require('express');
 var router = express.Router();
-var Habits = 
+var Users = require('../models/users');
+var Auth = require('../models/auth')
+var jwt = require('jsonwebtoken');
+require('dotenv').load();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/login', function(req, res, next) {
+  var token = req.body.token;
+  Auth.tokExc(token)
+  .then(function(profile) {
+    Users.login(profile)
+    .then(function(user) {
+
+      var cur_user = { token: jwt.sign( {user: user}, process.env.shh),
+                       user: user
+                     };
+
+      res.json(cur_user);
+    })
+    .catch(function(err) {
+      res.status(400);
+    });
+  })
+  .catch(function(err){
+    console.log(err);
+  });
 });
 
 module.exports = router;
